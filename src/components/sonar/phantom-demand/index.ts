b991cae7212cef2a0c8732b5ae85220154a444fb
simmysam3 <samfleming@gmail.com>
2026-05-28 07:07:04 -0700

    feat(pd-ui): SkuAutocomplete typeahead (fetcher-injected)

--- src/components/sonar/phantom-demand/index.ts
+++ src/components/sonar/phantom-demand/index.ts
@@ -3,3 +3,4 @@ export { SpotCheckTooltip } from './spot-check-tooltip';
 export { MesUnavailable } from './mes-unavailable';
 export { WeeksToHoldDropdown } from './weeks-to-hold-dropdown';
 export { VendorExcludeMultiSelect } from './vendor-exclude-multi-select';
+export { SkuAutocomplete } from './sku-autocomplete';
