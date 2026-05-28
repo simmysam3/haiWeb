5a579975332e93bdeaf9e0d4499a2556e4feda7f
simmysam3 <samfleming@gmail.com>
2026-05-28 07:13:44 -0700

    feat(pd-ui): BomTreeView hierarchical tree with status icons

--- src/components/sonar/phantom-demand/index.ts
+++ src/components/sonar/phantom-demand/index.ts
@@ -4,3 +4,4 @@ export { MesUnavailable } from './mes-unavailable';
 export { WeeksToHoldDropdown } from './weeks-to-hold-dropdown';
 export { VendorExcludeMultiSelect } from './vendor-exclude-multi-select';
 export { SkuAutocomplete } from './sku-autocomplete';
+export { BomTreeView } from './bom-tree-view';
